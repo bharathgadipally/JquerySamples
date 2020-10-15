121e609f9ddfc0cc2e72bc4577d73486f36f1724
bharathgadipally <bharath.gadipally@gmail.com>
2020-10-15 20:44:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,68 +1,70 @@
-var getSmallestSubString = function (s, t) {
+var getSmallestSubString = function (string1, string2) {
 	const seen = {};
 	const charOccurances = {};
 	let left = 0,
 		right = 0;
 	var res;
 
-	for (const char of t) {
+	for (const char of string2) {
 		charOccurances[char] = charOccurances[char] ? charOccurances[char] + 1 : 1;
 	}
 
-	while (!isCovered(seen, charOccurances, t) && right < s.length) {
-		let char = s[right];
+	while (!isCovered(seen, charOccurances, string2) && right < string1.length) {
+		let char = string1[right];
 		seen[char] = seen[char] ? seen[char] + 1 : 1;
 		right++;
 	}
-	if (!isCovered(seen, charOccurances, t)) {
+	if (!isCovered(seen, charOccurances, string2)) {
 		return "Not Found";
 	}
-	res = s.substring(left, right);
+	res = string1.substring(left, right);
 
 	while (
-		charOccurances[s[left]] === undefined ||
-		seen[s[left]] > charOccurances[s[left]]
+		charOccurances[string1[left]] === undefined ||
+		seen[string1[left]] > charOccurances[string1[left]]
 	) {
-		let char = s[left];
+		let char = string1[left];
 		seen[char]--;
 		left++;
 	}
 
-	const st = s.substring(left, right);
-	if (st.length < res.length) res = st;
+	const st = string1.substring(left, right);
+	if (st.length < res.length) {
+		res = st;
+	}
 
-	for (right; right <= s.length; right++) {
-		seen[s[right]]++;
+	for (right; right <= string1.length; right++) {
+		seen[string1[right]]++;
 
 		while (
-			charOccurances[s[left]] === undefined ||
-			seen[s[left]] > charOccurances[s[left]]
+			charOccurances[string1[left]] === undefined ||
+			seen[string1[left]] > charOccurances[string1[left]]
 		) {
-			let char = s[left];
+			let char = string1[left];
 			seen[char]--;
 			left++;
 		}
 
-		const substring = s.substring(left, right + 1);
+		const substring = string1.substring(left, right + 1);
 		if (substring.length < res.length) res = substring;
 	}
 
 	while (
-		charOccurances[s[right]] === undefined ||
-		seen[s[right]] > charOccurances[s[right]]
+		charOccurances[string1[right]] === undefined ||
+		seen[string1[right]] > charOccurances[string1[right]]
 	) {
-		let char = s[right];
+		let char = string1[right];
 		seen[char]--;
 		right--;
 	}
-	const substring = s.substring(left, right + 1);
+	const substring = string1.substring(left, right + 1);
 	if (substring.length < res.length) res = substring;
 
 	return res;
 };
 
-const isCovered = (seen, charOccurances, t) => {
-	for (const char of t) {
+const isCovered = (seen, charOccurances, string2) => {
+	for (const char of string2) {
 		if (!seen[char] || seen[char] < charOccurances[char]) return false;
 	}
 	return true;
